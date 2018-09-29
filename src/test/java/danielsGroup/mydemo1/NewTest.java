112caa3c03d46f73e8d4ed50cb376db469d0df2f
unknown <daniel@testim.io>
2018-09-29 11:26:35 +0300

    hello

---
+++ src/test/java/danielsGroup/mydemo1/NewTest.java
@@ -0,0 +1,17 @@
+package danielsGroup.mydemo1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void t() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:\\seleniumfiles\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://yoniflenner.net/Xamples/pizza");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.quit();
+  }
+}
